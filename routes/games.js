4bdaecdaf83d4fbc045a98de9d4935bf720796ef
Kentai Radiquum <kentai.waah@gmail.com>
2024-04-26 02:34:21 +0500

    Распределение функций на роуты

--- routes/games.js
+++ routes/games.js
@@ -1,13 +1,33 @@
 const gamesRouter = require("express").Router();
-const { getAllGamesMiddleware } = require("../middlewares/games");
-const {
-  sendAllGamesController,
-  deleteGameController,
-  addGameController,
-} = require("../controllers/games");
 
-gamesRouter.get("/games", getAllGamesMiddleware, sendAllGamesController);
-gamesRouter.post("/games", getAllGamesMiddleware, addGameController);
-gamesRouter.delete("/games/:id", getAllGamesMiddleware, deleteGameController);
+const {
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  findGameById,
+  deleteGame,
+} = require("../middlewares/games");
+const { sendAllGames, sendUpdatedGames } = require("../controllers/games");
+
+gamesRouter.get("/games", getAllGames, sendAllGames);
+
+gamesRouter.post(
+  "/games",
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  sendUpdatedGames
+);
+
+gamesRouter.delete(
+  "/games/:id",
+  getAllGames,
+  findGameById,
+  deleteGame,
+  updateGamesFile,
+  sendUpdatedGames
+);
 
 module.exports = gamesRouter;
